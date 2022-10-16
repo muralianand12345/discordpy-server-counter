eb1b6c69e98ddb108faa4340bd6722435df358aa
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2022-10-16 10:01:30 +0530

    : )

--- serverCounter.py
+++ serverCounter.py
@@ -16,10 +16,4 @@ async def on_ready():
     for guild in client.guilds:
         print(guild.name)
 
-#client.run('ODcxODA4NDI2MjQwNTIwMjUz.YQgs3g.no-hyi7r-F-FH4QYp3RPbxlBwT0') #MrBot's Token
-client.run('Nzc1NzEzMTA3NDQ3MjUwOTk1.X6qVIA.T-HS9T2CLHAPJPYjKtP_xpN0zdM') #Tony's Token
-#client.run('OTMyNjU0OTE0MzQwMDI4NDY2.YeWIkw.giASQUx6XlyRtrocIUF2NStgVW0')
-#client.run('OTQyMDgwNDY3MjQxNDEwNjMw.YgfSzg.oBwEgCwk5-3QiaVQ9qFVrbFnE0A')#elitex's Token
-
-
-#client.run("OTM3MDA5NTExMzU5NzMzNzgw.YfVgHA.7RfhrkqAXNKI4mQFfE9beWN2Xkw")#Minecraft's Token
+client.run('') #Tony's Token
